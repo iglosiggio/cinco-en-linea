696d99ad59ba398e5f24fa32800df0c04149845b
Ignacio Losiggio iglosiggio <iglosiggio@gmail.com>
2013-10-31 11:43:44 -0300

    Fixeando pequeño bug

--- cinco-en-linea/Logica.cs
+++ cinco-en-linea/Logica.cs
@@ -16,15 +16,15 @@ namespace Logica
         void gano()
         {
             int[][] data = {
-                new int[] {1, 0, 0, 10, 0, 15}, // Avanzo en x
-                new int[] {0, 1, 0, 15, 0, 10}, // Avanzo en y
+                new int[] {1, 0, 0, 10, 0, 14}, // Avanzo en x
+                new int[] {0, 1, 0, 14, 0, 10}, // Avanzo en y
                 new int[] {1, 1, 0, 10, 0, 10}, // Avanzo en x, y
-                new int[] {-1, 1, 4, 15, 0, 10} // Retrocedo en x, avanzo en y
+                new int[] {-1, 1, 4, 14, 0, 10} // Retrocedo en x, avanzo en y
             };
             foreach(int[] Config in data)
             {
-                for (int x = Config[2]; x < Config[3]; x++)
-                    for (int y = Config[4]; y < Config[5]; y++)
+                for (int x = Config[2]; x <= Config[3]; x++)
+                    for (int y = Config[4]; y <= Config[5]; y++)
                     {
                         int j = tablero[x, y];
                         if (j == 0)
