141779d39637edd5b83f4ce6b362db6ef705182d
Ignacio Losiggio iglosiggio <iglosiggio@gmail.com>
2013-11-05 11:13:51 -0300

    Reorganización mayor de la GUI, falta bastante para acomodar todo.. mergeo en la branch de lógica para trabajar allá la GUI.

--- cinco-en-linea/Form1.Designer.cs
+++ cinco-en-linea/Form1.Designer.cs
@@ -27,86 +27,33 @@
 		/// </summary>
 		private void InitializeComponent ()
 		{
-			
-			// Tablero hecho a mano
-			this.tablero = new Logica.Tablero ();
+            this.tablero_Graphics1 = new cinco_en_linea.Tablero_Graphics();
+            this.SuspendLayout();
+            // 
+            // tablero_Graphics1
+            // 
+            this.tablero_Graphics1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tablero_Graphics1.Location = new System.Drawing.Point(0, 0);
+            this.tablero_Graphics1.Name = "tablero_Graphics1";
+            this.tablero_Graphics1.Size = new System.Drawing.Size(329, 325);
+            this.tablero_Graphics1.TabIndex = 0;
+            this.tablero_Graphics1.Text = "tablero_Graphics1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 325);
+            this.Controls.Add(this.tablero_Graphics1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
 
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer ();
-			this.tablero_Graphics1 = new cinco_en_linea.Tablero_Graphics ();
-			this.hovertable1 = new cinco_en_linea.Hovertable ();
-			this.splitContainer1.Panel1.SuspendLayout ();
-			this.splitContainer1.Panel2.SuspendLayout ();
-			this.splitContainer1.SuspendLayout ();
-			this.SuspendLayout ();
-			// 
-			// splitContainer1
-			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point (0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add (this.tablero_Graphics1);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add (this.hovertable1);
-			this.splitContainer1.Size = new System.Drawing.Size (298, 319);
-			this.splitContainer1.SplitterDistance = 278;
-			this.splitContainer1.TabIndex = 0;
-			// 
-			// tablero_Graphics1
-			// 
-			this.tablero_Graphics1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tablero_Graphics1.Location = new System.Drawing.Point (0, 0);
-			this.tablero_Graphics1.Name = "tablero_Graphics1";
-			this.tablero_Graphics1.Size = new System.Drawing.Size (298, 278);
-			this.tablero_Graphics1.TabIndex = 0;
-			this.tablero_Graphics1.Text = "tablero_Graphics1";
-			this.tablero_Graphics1.ITablero = tablero;
-			// 
-			// hovertable1
-			// 
-			this.hovertable1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.hovertable1.Location = new System.Drawing.Point (0, 0);
-			this.hovertable1.Name = "hovertable1";
-			this.hovertable1.Size = new System.Drawing.Size (298, 37);
-			this.hovertable1.TabIndex = 0;
-			this.hovertable1.Text = "hovertable1";
-			this.hovertable1.HoverColumn += this.tablero_Graphics1.SeleccionarColumna;
-			this.hovertable1.MouseLeave += this.tablero_Graphics1.Hovertable_Leave;
-			this.hovertable1.MouseClick += this.tablero_Graphics1.Hovertable_Click;
-			//Tablero hecho a mano
-			this.tablero.CambioTurno += this.tablero_Graphics1.NextTurn;
-			this.tablero.CambioTurno += this.hovertable1.nextTurn;
-			this.tablero.Gano += Provisorio;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size (298, 319);
-			this.Controls.Add (this.splitContainer1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.splitContainer1.Panel1.ResumeLayout (false);
-			this.splitContainer1.Panel2.ResumeLayout (false);
-			this.splitContainer1.ResumeLayout (false);
-			this.ResumeLayout (false);
 		}
 
         #endregion
 
-		private System.Windows.Forms.SplitContainer splitContainer1;
-		private Hovertable hovertable1;
-		private Tablero_Graphics tablero_Graphics1;
-		private Logica.Tablero tablero;
-        
-        void Provisorio(object s, Logica.Ganador e) {
-            System.Windows.Forms.MessageBox.Show(System.String.Format("Ganó {0}", e.Jugador));
-        }
+        private Tablero_Graphics tablero_Graphics1;
 	}
 }
 
--- cinco-en-linea/Tablero_Graphics.cs
+++ cinco-en-linea/Tablero_Graphics.cs
@@ -13,76 +13,97 @@ namespace cinco_en_linea
 {
 	public partial class Tablero_Graphics : Control
 	{
+        // HOVERTABLE: DATA
+        enum Estados
+        {
+            Alone,
+            Entering,
+            Hovering,
+            Down,
+            Leaving
+        }
+        Color[,] cursorColor = {
+			{ Color.IndianRed, Color.DarkRed },
+			{ Color.RoyalBlue, Color.DarkBlue }
+		};
+        Int32 Jugador = 0;
+        Estados currentStatus;
+        ColorBlend animationColor;
+
+        // TABLER: DATA
 		Brush[] Colores = {
             Brushes.LightGray,
             Brushes.Pink,
-            Brushes.LightBlue
+            Brushes.LightSkyBlue
         };
 		Pen[] Líneas = {
             Pens.DarkGray,
             Pens.DeepPink,
-            Pens.LightSkyBlue
+            Pens.RoyalBlue
         };
 		Brush Selection = new SolidBrush (Color.Gray);
 		Animation ColumnaSeleccionada;
 		Timer animationTimer;
 		Int32 SColumna = 20;
-		public Tablero ITablero;
 
-		public void SeleccionarColumna (object sender, HovertableEventArgs e)
-		{
-			Rectangle Col = Columna (e.Columna);
-			if (ColumnaSeleccionada.Rect.Height == 0) {
-				ColumnaSeleccionada.Change (new Rectangle (Col.X, 0, Col.Width, 0), 1);
-				ColumnaSeleccionada.Next ();
-				ColumnaSeleccionada.Change (Col, 10);
-			}
-			else ColumnaSeleccionada.Change (Col, 10);
-			SColumna = e.Columna;
-		}
+        Int32 Margen;
+        Int32 Ficha_Alto;
+        Int32 Ficha_Ancho;
+        Int32 Columna_Ancho;
+
+		public Tablero MiTablero;
 
 		Rectangle Columna (Int32 col)
 		{
-			Int32 Margen = (ClientSize.Width - (((ClientSize.Width - 5 * 16) / 15) * 15 + 5 * 16));
-			Int32 Ancho = (ClientSize.Width - Margen) / 15;
-			return new Rectangle (Ancho * col + Margen / 2 + 2, 0, Ancho + 2, ClientSize.Height);
+			return new Rectangle (Columna_Ancho * col + Margen + 2, 0, Columna_Ancho + 2, ClientSize.Height);
 		}
 
 		void Tick (object sender, EventArgs e)
 		{
 			Invalidate ();
 		}
-
-		public void Hovertable_Leave (object sender, EventArgs e)
-		{
-			SColumna = 20;
-			Rectangle R = ColumnaSeleccionada.Rect;
-			ColumnaSeleccionada.Change (new Rectangle (R.Location, new Size (R.Width, 0)), 10);
-		}
-
-		public void Hovertable_Click (object sender, EventArgs e)
-		{
-			try {
-				ITablero.meterFicha (SColumna);
-			} catch (Exception ex) {
-				MessageBox.Show (ex.Message);
-			}
-		}
+        void CambioTurno(object s, Turno e)
+        {
+            Jugador = e.Jugador - 1;
+            animationColor.ChangeBlend(cursorColor[Jugador, 0], 7);
+        }
 
 		public void NextTurn (object sender, Turno e)
 		{
 			// TODO: Animaciones and shit 
 		}
 
+        public void Ganó(object sender, Ganador e)
+        {
+            MessageBox.Show(String.Format("Ganó el jugador {0}", e.Jugador));
+        }
+
 		public Tablero_Graphics ()
 		{
+            // HOVERDATA: CTOR
+            currentStatus = Estados.Alone;
+            DoubleBuffered = true;
+            animationColor = new ColorBlend(BackColor, Color.Red, 20);
+
+            // TABLERO: CTOR
 			DoubleBuffered = true;
 			ColumnaSeleccionada = new Animation (new Rectangle (0, 0, 0, 0), new Rectangle (0, 0, 0, 0), 0);
 			animationTimer = new Timer ();
 			animationTimer.Interval = 10;
-			animationTimer.Tick += Tick;
+			animationTimer.Tick += new EventHandler(Tick);
+            animationTimer.Tick += new EventHandler(doMagic);
 			animationTimer.Start ();
+
+            MiTablero = new Tablero();
+            MiTablero.CambioTurno += new EventHandler<Turno>(CambioTurno);
+            MiTablero.Gano += new EventHandler<Ganador>(Ganó);
+
 			InitializeComponent ();
+
+            Margen = (ClientSize.Width - (((ClientSize.Width - 5 * 16) / 15) * 15 + 5 * 16)) / 2;
+            Ficha_Alto = (ClientSize.Height - 5 * 16) / 15;
+            Ficha_Ancho = (ClientSize.Width - 5 * 16) / 15;
+            Columna_Ancho = (ClientSize.Width - Margen * 2) / 15;
 		}
 
 		protected override void OnPaint (PaintEventArgs pe)
@@ -90,28 +111,125 @@ namespace cinco_en_linea
 			base.OnPaint (pe);
 
 			// Data de dibujo
-			Int32 Alto = (ClientSize.Height - 5 * 16) / 15;
-			Int32 Ancho = (ClientSize.Width - 5 * 16) / 15;
-			Int32 Margen = (ClientSize.Width - (Ancho * 15 + 5 * 16)) / 2;
 			pe.Graphics.FillRectangle (Selection, ColumnaSeleccionada.Next ());
 			for (int i = 0; i < 15; i ++)
 				for (int j = 0; j < 15; j ++) {
-					Rectangle R = new Rectangle ((Ancho + 5) * j + Margen + 5, (Alto + 5) * i + 5, Ancho, Alto);
-					pe.Graphics.FillEllipse (Colores [ITablero [j, i]], R);
-					pe.Graphics.DrawEllipse (Líneas [ITablero [j, i]], R);
+                    Rectangle R = new Rectangle((Ficha_Ancho + 5) * j + Margen + 5, (Ficha_Alto + 5) * i + 5, Ficha_Ancho, Ficha_Alto);
+					pe.Graphics.FillEllipse (Colores [MiTablero [j, i]], R);
+					pe.Graphics.DrawEllipse (Líneas [MiTablero [j, i]], R);
 				}
 			try {
-				Rectangle actual = new Rectangle ((Ancho + 5) * SColumna + Margen + 5, (Alto + 5) * ITablero.ultimoLugar (SColumna) + 5, Ancho, Alto);
+                Rectangle actual = new Rectangle((Ficha_Ancho + 5) * SColumna + Margen + 5, (Ficha_Alto + 5) * MiTablero.ultimoLugar(SColumna) + 5, Ficha_Ancho, Ficha_Alto);
 				pe.Graphics.FillEllipse (Brushes.LimeGreen, actual);
 				pe.Graphics.DrawEllipse (Pens.Green, actual);
 			} catch (Exception) {
 			}
+
+            // HOVERTABLE: DRAW
+            Point start = PointToClient(MousePosition);
+            //PROVISORIO
+            GraphicsPath area = new GraphicsPath();
+            //area.AddEllipse(new Rectangle(start, new Size(50, 50)));
+            area.AddEllipse (new Rectangle (new Point (start.X - 25, ClientSize.Height - 25), new Size (50, 50)));
+
+            PathGradientBrush dibu = new PathGradientBrush(area);
+            dibu.CenterColor = animationColor.O;
+            dibu.SurroundColors = new Color[] { Color.FromArgb(0) };
+            dibu.CenterPoint = new Point(start.X, ClientSize.Height);
+            //dibu.CenterPoint = new Point (start.X + 25, ClientSize.Height);
+
+            pe.Graphics.FillPath(dibu, area);
+            dibu.Dispose();
+            area.Dispose();
 		}
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            Rectangle Col = ColumnaSeleccionada.Rect;
+            currentStatus = Estados.Entering;
+            ColumnaSeleccionada.Change(new Rectangle(Col.X, 0, Col.Width, 0), 1);
+            ColumnaSeleccionada.Next();
+
+            animationColor.ChangeBlend(cursorColor[Jugador, 0], 30);
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            currentStatus = Estados.Leaving;
+            animationColor.ChangeBlend(BackColor, 7);
+
+            SColumna = 20;
+            Rectangle R = ColumnaSeleccionada.Rect;
+            ColumnaSeleccionada.Change(new Rectangle(R.Location, new Size(R.Width, 0)), 10);
+            base.OnMouseLeave(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            Int32 PColumna = (e.X - Margen) / Columna_Ancho;
+            if (PColumna < 15 && PColumna >= 0)
+            {
+                SColumna = PColumna;
+                ColumnaSeleccionada.Change(Columna(SColumna), 10);
+            }
+            base.OnMouseMove(e);
+        }
 
 		protected override void OnSizeChanged (EventArgs e)
 		{
 			base.OnSizeChanged (e);
-			Invalidate ();
+            Margen = (ClientSize.Width - (((ClientSize.Width - 5 * 16) / 15) * 15 + 5 * 16)) / 2;
+            Ficha_Alto = (ClientSize.Height - 5 * 16) / 15;
+            Ficha_Ancho = (ClientSize.Width - 5 * 16) / 15;
+            Columna_Ancho = (ClientSize.Width - Margen * 2) / 15;
 		}
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            try
+            {
+                MiTablero.meterFicha(SColumna);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void doMagic(object sender, EventArgs e)
+        {
+            switch (currentStatus)
+            {
+                case Estados.Alone:
+                    return;
+                case Estados.Entering:
+                    if (animationColor.Ready)
+                        currentStatus = Estados.Hovering;
+                    else
+                        animationColor.Next();
+                    break;
+                case Estados.Hovering:
+                    if (animationColor.Ready)
+                        if (animationColor.O.ToArgb() == cursorColor[Jugador, 0].ToArgb())
+                            animationColor.ChangeBlend(cursorColor[Jugador, 1], 15);
+                        else if (animationColor.O.ToArgb() == cursorColor[Jugador, 1].ToArgb())
+                            animationColor.ChangeBlend(cursorColor[Jugador, 0], 15);
+                    animationColor.Next();
+                    break;
+                case Estados.Down:
+                    if (!animationColor.Ready)
+                        animationColor.Next();
+                    break;
+                case Estados.Leaving:
+                    if (animationColor.Ready)
+                        currentStatus = Estados.Alone;
+                    else
+                        animationColor.Next();
+                    break;
+            }
+            Invalidate();
+        }
 	}
 }
