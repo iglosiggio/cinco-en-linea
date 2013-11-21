c48e9c4c86d7b92a279760985e9c3891259f92a2
Ignacio Losiggio <iglosiggio@gmail.com>
2013-11-21 10:40:18 -0300

    Bot en dificultad media

--- cinco-en-linea/Logica.cs
+++ cinco-en-linea/Logica.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace Logica
 {
     public enum Dificultad { Fácil, Medio, Difícil, PvP };
-	public class Tablero
+	public class Tablero : ICloneable
 	{
 		Int32[,] tablero;
 		Int32 Jugador = 1;
@@ -20,50 +20,71 @@ namespace Logica
                 case Dificultad.Fácil:
                     Jugador2 = new BotFácil(this);
                     break;
+                case Dificultad.Difícil:
+                    Jugador2 = new BotDifícil(this);
+                    break;
                 case Dificultad.PvP:
                     Jugador2 = null;
 					break;
             }
 			tablero = new int[15, 15];
 		}
+        Tablero(Int32[,] _tablero, Int32 _Jugador, Bot _Jugador2)
+        {
+            tablero = _tablero;
+            Jugador = _Jugador;
+            Jugador2 = _Jugador2;
+        }
 
-		void gano ()
-		{
-			int[][] data = {
+        public Int32 NFichas(Int32 N)
+        {
+            int[][] data = {
                 new int[] {1, 0, 0, 10, 0, 14}, // Avanzo en x
                 new int[] {0, 1, 0, 14, 0, 10}, // Avanzo en y
                 new int[] {1, 1, 0, 10, 0, 10}, // Avanzo en x, y
                 new int[] {-1, 1, 4, 14, 0, 10} // Retrocedo en x, avanzo en y
             };
-			foreach (int[] Config in data) {
-				for (int x = Config[2]; x <= Config[3]; x++)
-					for (int y = Config[4]; y <= Config[5]; y++) {
-						int j = tablero [x, y];
-						if (j == 0)
-							continue;
-						int c;
-						for (c = 0; c < 5; c++)
-							if (tablero [x + c * Config [0], y + c * Config [1]] != j)
-								break;
-						if (c == 5 && Gano != null)
-							Gano (this, new Ganador (tablero [x, y]));
-					}
-			}
+            foreach (int[] Config in data)
+            {
+                for (int x = Config[2]; x <= Config[3]; x++)
+                    for (int y = Config[4]; y <= Config[5]; y++)
+                    {
+                        int j = tablero[x, y];
+                        if (j == 0)
+                            continue;
+                        int c;
+                        for (c = 0; c < 5; c++)
+                            if (tablero[x + c * Config[0], y + c * Config[1]] != j)
+                                break;
+                        if (c == N)
+                            return tablero[x, y];
+                    }
+            }
+            return 0;
+        }
+
+		void gano ()
+		{
+            Int32 elGanador = NFichas(5);
+            if(Gano != null && elGanador != 0)
+                Gano(this, new Ganador(elGanador));
 		}
 
-		public void meterFicha (Int32 columna)
+		public Tablero meterFicha (Int32 columna)
 		{
 			Int32 fila = ultimoLugar (columna);
 			if(fila == -1)
 				throw new InvalidOperationException ("Columna completa");
 			tablero [columna, fila] = Jugador;
 			gano ();
-			CambioTurno (this, new Turno (Jugador, columna, fila));
+            if(CambioTurno != null)
+			    CambioTurno (this, new Turno (Jugador, columna, fila));
 			Jugador = Jugador == 1 ? 2 : 1;
             if (Jugador == 2 && Jugador2 != null)
             {
                 meterFicha(Jugador2.Turno());
             }
+            return this;
 		}
 
         public Boolean columnaLlena(Int32 columna)
@@ -85,6 +106,11 @@ namespace Logica
 		public Int32 this [Int32 X, Int32 Y] {
 			get { return tablero [X, Y]; }
 		}
+
+        public object Clone()
+        {
+            return new Tablero((Int32[,])tablero.Clone(), Jugador, null);
+        }
 	}
 
 	public class Ganador : EventArgs
@@ -136,6 +162,49 @@ namespace Logica
                 Columna = R.Next(0, 14);
             } while (T.columnaLlena(Columna));
             return Columna;
+        }
+    }
+    public class BotDifícil : Bot
+    {
+        public BotDifícil(Tablero _T) : base(_T) { }
+        public override Int32 Turno()
+        {
+            Int32 Columna;
+            Tablero CurrentTablero = (Tablero)T.Clone();
+            for (Columna = 0; CurrentTablero.columnaLlena(Columna); Columna++) ;
+            Int32[] MejorTurno = {0, Sum(CurrentTablero.meterFicha(Columna), 0)};
+            for (Columna = 1; Columna < 15; Columna++)
+            {
+                CurrentTablero = (Tablero)T.Clone();
+                try
+                {
+                    Int32[] CurrentTurno = { Columna, Sum(CurrentTablero.meterFicha(Columna), 1) };
+                    if (CurrentTurno[1] > MejorTurno[1])
+                        MejorTurno = CurrentTurno;
+                    else if (CurrentTurno[1] == MejorTurno[1] && new Random().Next() % 3 == 1)
+                        MejorTurno = CurrentTurno;
+                }
+                catch (Exception) { }
+            }
+            return MejorTurno[0];
+        }
+        Int32 Sum(Tablero ElTablero, Int32 Profundidad)
+        {
+            Int32 STotal = 0;
+            if(Profundidad == 3)
+                return 0;
+            for (Int32 Columna = 0; Columna < 15; Columna++)
+            {
+                Int32 N5 = ElTablero.NFichas(5);
+                Tablero NuevoTurno = (Tablero)ElTablero.Clone();
+                try
+                {
+                    STotal += Sum(NuevoTurno.meterFicha(Columna), Profundidad + 1);
+                    STotal += N5 != 0 ? N5 == 2 ? 60 / Profundidad : -900 / Profundidad : 0;
+                }
+                catch (Exception) { }
+            }
+            return STotal;
         }
     }
 }
