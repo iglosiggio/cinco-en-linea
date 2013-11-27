b41bab706e722760618872633864a85390fe4ac1
Ignacio Losiggio iglosiggio <iglosiggio@gmail.com>
2013-11-27 16:16:53 -0300

    Incluído cache en el bot alfa-beta

--- cinco-en-linea/Logica.cs
+++ cinco-en-linea/Logica.cs
@@ -8,23 +8,24 @@ namespace Logica
 	public class Tablero : ICloneable
 	{
 		Int32[,] tablero;
-		Int32 Jugador = 1;
+		public Int32 Jugador { get; private set; }
         Bot Jugador2;
 		public event EventHandler<Ganador> Gano;
 		public event EventHandler<Turno> CambioTurno;
 
 		public Tablero (Dificultad Dif)
 		{
+			Jugador = 1;
             switch (Dif)
             {
                 case Dificultad.Fácil:
-                    Jugador2 = new BotFácil(this);
+                    Jugador2 = new BotFácil(new Tablero(Logica.Dificultad.PvP));
                     break;
                 case Dificultad.Medio:
-                    Jugador2 = new BotAlfaBeta(this, 3);
+                    Jugador2 = new BotAlfaBeta(new Tablero(Logica.Dificultad.PvP), 4);
                     break;
 				case Dificultad.Difícil:
-					Jugador2 = new BotAlfaBeta(this, 5);
+					Jugador2 = new BotAlfaBeta(new Tablero(Logica.Dificultad.PvP), 7);
 					break;
                 case Dificultad.PvP:
                     Jugador2 = null;
@@ -82,10 +83,10 @@ namespace Logica
 			gano ();
             if(CambioTurno != null)
 			    CambioTurno (this, new Turno (Jugador, columna, fila));
-			Jugador = Jugador == 1 ? 2 : 1;
+			Jugador = (Jugador % 2) + 1;
             if (Jugador == 2 && Jugador2 != null)
             {
-                meterFicha(Jugador2.Turno());
+                meterFicha(Jugador2.Turno(columna));
             }
             return this;
 		}
@@ -148,7 +149,7 @@ namespace Logica
         {
             T = _T;
         }
-        public abstract Int32 Turno();
+        public abstract Int32 Turno(Int32 Previo);
     }
     public class BotFácil : Bot
     {
@@ -158,7 +159,7 @@ namespace Logica
         {
             R = new Random();
         }
-        public override int Turno()
+        public override int Turno(Int32 Previo)
         {
             Int32 Columna;
             do{
@@ -170,47 +171,28 @@ namespace Logica
     public class BotAlfaBeta : Bot
     {
 		Int32 Complejidad;
+		Explorador E;
         public BotAlfaBeta(Tablero _T, Int32 C) : base(_T)
 		{
 			Complejidad = C;
+			E = new Explorador(T);
 		}
-        public override Int32 Turno()
+        public override Int32 Turno(Int32 Previo)
         {
-			Pair<Int32, Int32> Best = new Pair<int, int>(0, -99999999);
-            for (Int32 Columna = 0; Columna < 15; Columna++)
-            {
-				Pair<Int32, Int32> P;
-				try {
-					P = Sum(((Tablero)T.Clone()).meterFicha(Columna), Complejidad, Columna, 2, -99999999, 99999999);
-                }
-                catch (Exception) {
-                    P = new Pair<Int32, Int32>(Columna, -99999999);
-                }
-                if (Best.B < P.B)
-                {
-					Best = P;
-                }
-            }
+			Pair<Int32, Int32> Best = Sum (E.Get(Previo), Complejidad, 2, -99999999, 99999999);
+			E = E.Get(Previo).Get(Best.A);
+			Console.WriteLine();
             return Best.A;
         }
-        Pair<Int32, Int32> Sum (Tablero ElTablero, Int32 Profundidad, Int32 Columna, Int32 Jugador, Int32 alfa, Int32 beta)
+        Pair<Int32, Int32> Sum (Explorador E, Int32 Profundidad, Int32 Jugador, Int32 alfa, Int32 beta)
 		{
+			if (E.P.B != 0 || Profundidad == 0)
+				return E.P;
 			Int32 RColumna = 0;
-			Int32 N5 = ElTablero.NFichas (5);
-			if (N5 == Jugador) {
-				return new Pair<Int32, Int32> (Columna, 10 * Profundidad + 1 / Jugador);
-			}
-			if (Profundidad == 0)
-				return new Pair<Int32, Int32> (Columna, 0);
-            for (Columna = 0; Columna < 15; Columna++)
+            for (Int32 Columna = 0; Columna < 15; Columna++)
             {
                 Pair<Int32, Int32> P;
-                try {
-                    P = Sum(((Tablero)ElTablero.Clone()).meterFicha(Columna), Profundidad - 1, Columna, (Jugador % 2) + 1, -beta, -alfa);
-                }
-                catch (Exception) {
-                    P = new Pair<Int32, Int32>(Columna, -99999999);
-                }
+                P = Sum(E.Get(Columna), Profundidad - 1, (Jugador % 2) + 1, -beta, -alfa);
                 if (alfa < P.B)
                 {
                     alfa = P.B;
@@ -218,9 +200,6 @@ namespace Logica
                 }
                 if (beta <= alfa)
                     break;
-				//for (int i = Profundidad; i >= 0; i--)
-				//	Console.Write("  ");
-				//Console.WriteLine("Alfa: {0}, Beta {1}, Columna {3}, Jugador {4}", alfa, beta, Columna, Profundidad, Jugador);
             }
             return new Pair<Int32,Int32> (RColumna, alfa);
         }
@@ -249,4 +228,44 @@ namespace Logica
 			return string.Format ("({0}; {1})", A, B);
 		}
     }
+	class Explorador
+	{
+		public Tablero T;
+		Explorador[] Futuros;
+		public Pair<Int32, Int32> P { get; private set; }
+		public Explorador (Tablero _T)
+		{
+			T = _T;
+			Futuros = new Explorador[15];
+		}
+		public Explorador (Tablero _T, Pair<Int32, Int32> _P)
+		{
+			T = _T;
+			Futuros = new Explorador[15];
+			P = _P;
+		}
+		public Explorador Get (Int32 N)
+		{
+			if (P.B != 0)
+				return this;
+			if (Futuros [N] != null) {
+				return Futuros [N];
+			}
+			if (N >= 15 || N < 0)
+				throw new ArgumentOutOfRangeException ();
+			Pair<Int32, Int32> NewP;
+			Tablero NewT;
+			try {
+				NewT = ((Tablero)T.Clone ()).meterFicha (N);
+				if (NewT.NFichas (5) == T.Jugador)
+					NewP = new Pair<Int32, Int32> (N, 10);
+				else
+					NewP = new Pair<Int32, Int32> (N, 0);
+			} catch (Exception) {
+				NewT = (Tablero)T.Clone();
+				NewP = new Pair<Int32, Int32> (N, -99999999);
+			}
+			return Futuros [N] = new Explorador (NewT, NewP);
+		}
+	}
 }
